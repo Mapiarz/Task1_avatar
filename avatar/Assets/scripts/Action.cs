a2b09c07fbe83dfa12a9e55e81aebe28fc540f0b
mNiechcial <36309995+mNiechcial@users.noreply.github.com>
2018-04-04 00:32:53 +0200

    Create
    Adding avatar from Kinect
    3 basic scripts with chaotic methods

---
+++ avatar/Assets/scripts/Action.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+void ReadData()
+{
+    try
+    {
+        while (WorkCondition)
+        {
+            Byte[] receiveBytes = DataGeneration.Gather(int bodyPart);
+            string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+            //ESP
+            Frame_lenght = 62;
+            //string returnData = Encoding.ASCII.GetString(receiveBytes);
+            Buffer.BlockCopy(receiveBytes, 4, sensor_udp, 0, Frame_lenght - 4 - 2);
+
+            time = sensor_udp[0]; //czas z pliku
+
+            //dane sensoryczne
+            for (int i = 0; i < 3; i++)
+            {
+                acc[i] = sensor_udp[i + 1];
+                mag[i] = sensor_udp[i + 4];
+                gyr[i] = sensor_udp[i + 7];
+            }
+
+            q.w = sensor_udp[10];
+            q.x = -sensor_udp[11];
+            q.y = -sensor_udp[13];
+            q.z = -sensor_udp[12];
+            //dane z udp dla 5 czujników, ale tylko i wyłącznie kawaterniony
+            int nr = 3 * 4;
+            /*
+                            q.w = data[nr + 0];
+                            q.x = data[nr + 1];
+                            q.y = data[nr + 2];
+                            q.z = data[nr + 3];
+              */
+        }
+    }
+    catch
+    {
+
+
+    }
+}
---
+++ avatar/Assets/scripts/AvatarController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+    private Animator animatorComponent = null;
+    protected Transform[] bones;
+
+
+
+    public Vector3 GetJointWorldPos(JointType jointType)
+    {
+        Vector3 jointPosition = DataSource.GetJointPosition(playerId, (int)jointType);
+        Vector3 worldPosition = new Vector3(
+            jointPosition.x - offsetPos.x,
+            //            jointPosition.y - offsetPos.y + kinectManager.sensorHeight,  //!! this should be better investigated .. 
+            jointPosition.y + offsetPos.y - kinectManager.sensorHeight,  //!! this workds better on my example 
+            !mirroredMovement && !posRelativeToCamera ? (-jointPosition.z - offsetPos.z) : (jointPosition.z - offsetPos.z));
+
+        Quaternion posRotation = mirroredMovement ? Quaternion.Euler(0f, 180f, 0f) * initialRotation : initialRotation;
+        worldPosition = posRotation * worldPosition;
+
+        return bodyRootPosition + worldPosition;
+    }
+
+}
+
+public enum JointType : int //source kinectInterop
+{
+    SpineBase = 0,
+    SpineMid = 1,
+    Neck = 2,
+    Head = 3,
+    ShoulderLeft = 4,
+    ElbowLeft = 5,
+    WristLeft = 6,
+    HandLeft = 7,
+    ShoulderRight = 8,
+    ElbowRight = 9,
+    WristRight = 10,
+    HandRight = 11,
+    HipLeft = 12,
+    KneeLeft = 13,
+    AnkleLeft = 14,
+    FootLeft = 15,
+    HipRight = 16,
+    KneeRight = 17,
+    AnkleRight = 18,
+    FootRight = 19,
+    SpineShoulder = 20,
+    HandTipLeft = 21,
+    ThumbLeft = 22,
+    HandTipRight = 23,
+    ThumbRight = 24
+    //Count = 25
+}
+protected static readonly Dictionary<KinectInterop.JointType, int> jointMap2boneIndex = new Dictionary<KinectInterop.JointType, int>
+    {
+        {KinectInterop.JointType.SpineBase, 0},
+        {KinectInterop.JointType.SpineMid, 1},
+        {KinectInterop.JointType.SpineShoulder, 2},
+        {KinectInterop.JointType.Neck, 3},
+        {KinectInterop.JointType.Head, 4},
+
+        {KinectInterop.JointType.ShoulderLeft, 5},
+        {KinectInterop.JointType.ElbowLeft, 6},
+        {KinectInterop.JointType.WristLeft, 7},
+        {KinectInterop.JointType.HandLeft, 8},
+
+        {KinectInterop.JointType.HandTipLeft, 9},
+        {KinectInterop.JointType.ThumbLeft, 10},
+
+        {KinectInterop.JointType.ShoulderRight, 11},
+        {KinectInterop.JointType.ElbowRight, 12},
+        {KinectInterop.JointType.WristRight, 13},
+        {KinectInterop.JointType.HandRight, 14},
+
+        {KinectInterop.JointType.HandTipRight, 15},
+        {KinectInterop.JointType.ThumbRight, 16},
+
+        {KinectInterop.JointType.HipLeft, 17},
+        {KinectInterop.JointType.KneeLeft, 18},
+        {KinectInterop.JointType.AnkleLeft, 19},
+        {KinectInterop.JointType.FootLeft, 20},
+
+        {KinectInterop.JointType.HipRight, 21},
+        {KinectInterop.JointType.KneeRight, 22},
+        {KinectInterop.JointType.AnkleRight, 23},
+        {KinectInterop.JointType.FootRight, 24},
+    };
