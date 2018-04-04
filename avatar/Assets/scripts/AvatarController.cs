fefc190d5e8b6a12b8166bc33f1fe3b1e3c01bfd
mNiechcial <36309995+mNiechcial@users.noreply.github.com>
2018-04-04 23:12:24 +0200

    Add
    Definitions of joints, basic methods for quaternion manipulation

--- avatar/Assets/scripts/AvatarController.cs
+++ avatar/Assets/scripts/AvatarController.cs
@@ -6,86 +6,127 @@ public class AvatarController : MonoBehaviour
 {
     private Animator animatorComponent = null;
     protected Transform[] bones;
+    protected Quaternion[] initialRotations;
+    protected Quaternion[] localRotations;
 
-
-
-    public Vector3 GetJointWorldPos(JointType jointType)
+    protected virtual void MapBones()
     {
-        Vector3 jointPosition = DataSource.GetJointPosition(playerId, (int)jointType);
-        Vector3 worldPosition = new Vector3(
-            jointPosition.x - offsetPos.x,
-            //            jointPosition.y - offsetPos.y + kinectManager.sensorHeight,  //!! this should be better investigated .. 
-            jointPosition.y + offsetPos.y - kinectManager.sensorHeight,  //!! this workds better on my example 
-            !mirroredMovement && !posRelativeToCamera ? (-jointPosition.z - offsetPos.z) : (jointPosition.z - offsetPos.z));
+        for (int boneIndex = 0; boneIndex < bones.Length; boneIndex++)
+        {
+            if (!boneIndex2MecanimMap.ContainsKey(boneIndex))
+                continue;
 
-        Quaternion posRotation = mirroredMovement ? Quaternion.Euler(0f, 180f, 0f) * initialRotation : initialRotation;
-        worldPosition = posRotation * worldPosition;
+            bones[boneIndex] = animatorComponent ? animatorComponent.GetBoneTransform(boneIndex2MecanimMap[boneIndex]) : null;
+        }
+    }
+        public void Awake()
+    {
+        bones = new Transform[10];
+        initialRotations = new Quaternion[bones.Length];
+        localRotations = new Quaternion[bones.Length];
+        GetInitialRotations();
+        animatorComponent = GetComponent<Animator>();
+        }
 
-        return bodyRootPosition + worldPosition;
+    public enum JointType : int
+    {
+        ShoulderLeft = 1,
+        ElbowLeft = 2,
+        ShoulderRight = 3,
+        ElbowRight = 4,
+        HipLeft = 5,
+        KneeLeft = 6,
+        AnkleLeft = 7,
+        HipRight = 8,
+        KneeRight = 9,
+        AnkleRight = 10,
     }
 
-}
-
-public enum JointType : int //source kinectInterop
-{
-    SpineBase = 0,
-    SpineMid = 1,
-    Neck = 2,
-    Head = 3,
-    ShoulderLeft = 4,
-    ElbowLeft = 5,
-    WristLeft = 6,
-    HandLeft = 7,
-    ShoulderRight = 8,
-    ElbowRight = 9,
-    WristRight = 10,
-    HandRight = 11,
-    HipLeft = 12,
-    KneeLeft = 13,
-    AnkleLeft = 14,
-    FootLeft = 15,
-    HipRight = 16,
-    KneeRight = 17,
-    AnkleRight = 18,
-    FootRight = 19,
-    SpineShoulder = 20,
-    HandTipLeft = 21,
-    ThumbLeft = 22,
-    HandTipRight = 23,
-    ThumbRight = 24
-    //Count = 25
-}
-protected static readonly Dictionary<KinectInterop.JointType, int> jointMap2boneIndex = new Dictionary<KinectInterop.JointType, int>
+    protected void GetInitialRotations()
     {
-        {KinectInterop.JointType.SpineBase, 0},
-        {KinectInterop.JointType.SpineMid, 1},
-        {KinectInterop.JointType.SpineShoulder, 2},
-        {KinectInterop.JointType.Neck, 3},
-        {KinectInterop.JointType.Head, 4},
+        for (int i = 0; i < bones.Length; i++)
+        {
+            if (bones[i] != null)
+            {
+                initialRotations[i] = bones[i].rotation;
+                localRotations[i] = bones[i].localRotation;
+            }
+        }
+    }
+    protected Quaternion Data2AvatarRot(Quaternion jointRotation, int boneIndex)
+    {
+        Quaternion newRotation = jointRotation * initialRotations[boneIndex];
 
-        {KinectInterop.JointType.ShoulderLeft, 5},
-        {KinectInterop.JointType.ElbowLeft, 6},
-        {KinectInterop.JointType.WristLeft, 7},
-        {KinectInterop.JointType.HandLeft, 8},
+        return newRotation;
+    }
+    public Quaternion GetJointOrientation(int joint)
+    {
+     
+            int index = dictUserIdToIndex[userId];
 
-        {KinectInterop.JointType.HandTipLeft, 9},
-        {KinectInterop.JointType.ThumbLeft, 10},
+            if (index >= 0 && index < sensorData.bodyCount &&
+               bodyFrame.bodyData[index].bIsTracked != 0)
+            {
+                if (flip)
+                    return bodyFrame.bodyData[index].joint[joint].normalRotation;
+                else
+                    return bodyFrame.bodyData[index].joint[joint].mirroredRotation;
+            }
 
-        {KinectInterop.JointType.ShoulderRight, 11},
-        {KinectInterop.JointType.ElbowRight, 12},
-        {KinectInterop.JointType.WristRight, 13},
-        {KinectInterop.JointType.HandRight, 14},
+        return Quaternion.identity;
+    }
+    protected void TransformBone(JointType joint, int boneIndex, bool flip)
+    {
+        Transform boneTransform = bones[boneIndex];
 
-        {KinectInterop.JointType.HandTipRight, 15},
-        {KinectInterop.JointType.ThumbRight, 16},
+        int iJoint = (int)joint;
 
-        {KinectInterop.JointType.HipLeft, 17},
-        {KinectInterop.JointType.KneeLeft, 18},
-        {KinectInterop.JointType.AnkleLeft, 19},
-        {KinectInterop.JointType.FootLeft, 20},
+        Quaternion jointRotation = GetJointOrientation(iJoint);
+        // calculate the new orientation
+        Quaternion newRotation = Data2AvatarRot(jointRotation, boneIndex);
+            boneTransform.rotation = newRotation;
+    }
 
-        {KinectInterop.JointType.HipRight, 21},
-        {KinectInterop.JointType.KneeRight, 22},
-        {KinectInterop.JointType.AnkleRight, 23},
-        {KinectInterop.JointType.FootRight, 24},
+
+
+    protected static readonly Dictionary<int, HumanBodyBones> boneIndex2MecanimMap = new Dictionary<int, HumanBodyBones>
+    {
+        {0, HumanBodyBones.Hips},
+        {1, HumanBodyBones.Spine},
+//        {2, HumanBodyBones.Chest},
+		{3, HumanBodyBones.Neck},
+//		{4, HumanBodyBones.Head},
+		
+		{5, HumanBodyBones.LeftUpperArm},
+        {6, HumanBodyBones.LeftLowerArm},
+        {7, HumanBodyBones.LeftHand},
+
+		
+		{11, HumanBodyBones.RightUpperArm},
+        {12, HumanBodyBones.RightLowerArm},
+        {13, HumanBodyBones.RightHand},
+//		{14, HumanBodyBones.RightIndexProximal},
+//		{15, HumanBodyBones.RightIndexIntermediate},
+//		{16, HumanBodyBones.RightThumbProximal},
+		
+		{17, HumanBodyBones.LeftUpperLeg},
+        {18, HumanBodyBones.LeftLowerLeg},
+        {19, HumanBodyBones.LeftFoot},
+//		{20, HumanBodyBones.LeftToes},
+		
+		{21, HumanBodyBones.RightUpperLeg},
+        {22, HumanBodyBones.RightLowerLeg},
+        {23, HumanBodyBones.RightFoot},
+//		{24, HumanBodyBones.RightToes},
+		
+		{25, HumanBodyBones.LeftShoulder},
+        {26, HumanBodyBones.RightShoulder},
+        {27, HumanBodyBones.LeftIndexProximal},
+        {28, HumanBodyBones.RightIndexProximal},
+        {29, HumanBodyBones.LeftThumbProximal},
+        {30, HumanBodyBones.RightThumbProximal},
     };
+
+}
+
+
